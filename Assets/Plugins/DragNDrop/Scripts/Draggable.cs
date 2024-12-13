556daf50967541bbac1b0e51151d904bb3b88859
Gabriel Massé <orkaan974@gmail.com>
2024-12-13 22:48:25 +0400

    Added custom editor and refactored files to make plugin

---
+++ Assets/Plugins/DragNDrop/Scripts/Draggable.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.KherusEmporium.DragNDrop {
+
+	/// <summary>
+	/// Class for the objects dragged and contained
+	/// </summary>
+	[Serializable]
+	public class Draggable : MonoBehaviour {
+
+		public Container previousContainer = null;
+		private int defaultLayer;
+		private Transform defaultParent;
+		private Vector3 defaultPos;
+
+		private void Start() {
+			defaultLayer = gameObject.layer;
+			defaultPos = transform.localPosition;
+			defaultParent= transform.parent;
+		}
+
+		public void ReturnToPreviousContainer() {
+			if (previousContainer != null) {
+				previousContainer.Add(this);
+			}
+			else ReturnToStartPos();
+
+		}
+
+		public void ReturnToStartPos() {
+			transform.SetParent(defaultParent);
+			transform.localPosition = defaultPos;
+			gameObject.layer = defaultLayer;
+		}
+
+		public void Grab() {
+			gameObject.layer = 2;
+		}
+
+		public void Drop(bool canDropAnywhere, bool inContainer) {
+			if (!canDropAnywhere && !inContainer) ReturnToPreviousContainer();
+			else {
+				gameObject.layer = defaultLayer;
+				previousContainer = null;
+			}
+		}
+	}
+}
